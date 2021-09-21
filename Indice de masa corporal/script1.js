a8217d75e660582eaa53d8a1637ea02f8ae673c2
ingrid <ingridvasquezo99@gmail.com>
2021-09-20 22:10:31 -0500

    master

--- Indice de masa corporal/script1.js
+++ Indice de masa corporal/script1.js
@@ -1,12 +1,6 @@
 
-
-//function iniciar(){
- //var btnCalcular = document.getElementById("btnCalcular")
- //btnCalcular.addEventListener("click", clickBtncalcular());
-//}
-var x = clickBtncalcular();
 function clickBtncalcular(){
-    alert("se esta ejecutando");
+ 
     var txtPeso = document.getElementById("txtPeso");
     var peso = txtPeso.value;
     var imc = 0;
@@ -15,6 +9,6 @@ function clickBtncalcular(){
     var altura = txtAltura.value;
     imc = peso / (altura * altura);
     document.getElementById("resultado").innerText=imc;
-    return imc;
+    
 }
-document.getElementById("resultado").innerText=x;
+
